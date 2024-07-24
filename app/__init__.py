e76217d94786634f890ea5aa9ea38dc85f676b69
lirabenjamin <lira.benjamin@gmail.com>
2024-07-24 14:41:50 -0400

    chore: Add MongoDB connection setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(SECRET_KEY='your_secret_key')
+
+    # MongoDB connection setup
+    mongo_uri = os.getenv('MONGO_URI')
+    client = MongoClient(mongo_uri)
+    app.db = client['email_rating_app']
 
     with app.app_context():
         from . import routes
