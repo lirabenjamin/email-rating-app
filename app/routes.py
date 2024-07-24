fe871c71fe8dfcff5f47f6479a98d595c95142ef
lirabenjamin <lira.benjamin@gmail.com>
2024-07-24 15:01:20 -0400

    chore: Remove index route and refactor app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -9,14 +9,6 @@ with open('instance/emails.json', 'r') as f:
 # Track rated emails
 rated_emails = {ra: [] for ra in ['RA1', 'RA2', 'RA3', 'RA4', 'RA5', 'RA6', 'RA7', 'RA8', 'RA9', 'RA10']}
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        ra_name = request.form['ra_name']
-        session['ra_name'] = ra_name
-        return redirect(url_for('rate_email'))
-    return render_template('index.html')
-
 @app.route('/rate', methods=['GET', 'POST'])
 def rate_email():
     ra_name = session.get('ra_name')
@@ -48,3 +40,4 @@ def rate_email():
 
     email_to_rate = random.choice(available_emails)
     return render_template('rating.html', email=email_to_rate)
+
